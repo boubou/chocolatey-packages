5ea4f22cfd5d89e0a59910129ec5f6e9c3fd56d7
Sébastien Boulianne <10491809+boubou@users.noreply.github.com>
2024-07-03 09:08:36 -0400

    Add files via upload

---
+++ jabra-direct/tools/chocolateyinstall.ps1
@@ -0,0 +1,32 @@
+﻿# jabra-direct install
+
+$ErrorActionPreference = 'Stop';
+
+$toolsDir            = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)"
+$PackageParameters   = Get-PackageParameters
+$urlPackage = 'https://jabraxpressonlineprdstor.blob.core.windows.net/jdo/JabraDirectSetup.exe'
+$checksumPackage = '587966AA7EA47E9E24A2EA814B63FBBF8215DB42C8126C158D4A20AC2B235331'
+$checksumTypePackage = 'sha256'
+
+Import-Module -Name "$($toolsDir)\helpers.ps1"
+
+$packageArgs = @{
+    packageName    = $env:ChocolateyPackageName
+    fileType       = 'EXE'
+    url            = $urlPackage
+    checksum       = $checksumPackage
+    checksumType   = $checksumTypePackage
+	silentArgs     = '/install /quiet /norestart'
+    validExitCodes = @(0, 1000, 1101)
+}
+
+Install-ChocolateyPackage @packageArgs
+
+if ($PackageParameters.CleanStartmenu) {
+	Remove-FileItem `
+		-Path "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Jabra\"
+    Install-ChocolateyShortcut `
+        -ShortcutFilePath "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Jabra Direct.lnk" `
+        -TargetPath "C:\Program Files (x86)\Jabra\Direct\JabraDirect.exe" `
+        -WorkDirectory "C:\Program Files (x86)\Jabra\Direct\"
+}
